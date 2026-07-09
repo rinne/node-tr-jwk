0dc4fbcfcaacc779731ed87e29fe90a232bab505
Timo J. Rinne <tri@iki.fi>
2026-07-09 23:07:27 +0300

    Added ML-DSA and ML-KEM key pair generation. Bumped version to 2.1.0

---
+++ ml-kem-key-gen.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const crypto = require('node:crypto');
+const { promisify } = require('node:util');
+
+const generateKeyPair = promisify(crypto.generateKeyPair);
+
+const variants = {
+    'ML-KEM-512': 'ml-kem-512',
+    'ML-KEM-768': 'ml-kem-768',
+    'ML-KEM-1024': 'ml-kem-1024'
+};
+
+function mlKemKeyGen(variant) {
+    if (! variants[variant]) {
+        throw new Error('Invalid ML-KEM variant');
+    }
+    const k = crypto.generateKeyPairSync(variants[variant], {});
+    const kid = crypto.randomUUID();
+    const secretKey = k.privateKey.export({ format: 'jwk' });
+    secretKey.kid = kid;
+    const publicKey = k.publicKey.export({ format: 'jwk' });
+    publicKey.kid = kid;
+    return { secretKey, publicKey };
+}
+
+async function mlKemKeyGenAsync(variant) {
+    if (! variants[variant]) {
+        throw new Error('Invalid ML-KEM variant');
+    }
+    const k = await generateKeyPair(variants[variant], {});
+    const kid = crypto.randomUUID();
+    const secretKey = k.privateKey.export({ format: 'jwk' });
+    secretKey.kid = kid;
+    const publicKey = k.publicKey.export({ format: 'jwk' });
+    publicKey.kid = kid;
+    return { secretKey, publicKey };
+}
+
+module.exports = { mlKemKeyGen, mlKemKeyGenAsync };
